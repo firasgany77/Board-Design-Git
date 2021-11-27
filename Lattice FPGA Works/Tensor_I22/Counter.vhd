ef5f00e0dac67cc12a5f0769aa41f9de4c251661
firas abdelghani <firasgany77>
2021-11-27 08:32:51 +0200

    Added example Code + HDL PDF

--- Lattice FPGA Works/Tensor_I22/Counter.vhd
+++ Lattice FPGA Works/Tensor_I22/Counter.vhd
@@ -36,3 +36,39 @@ BEGIN
 	END PROCESS;
 
 END counter_arch;
+
+
+
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.numeric_std.ALL;
+
+ENTITY Clock_Divider IS
+	PORT (
+		clk, reset : IN STD_LOGIC; --Clk = 50Mhz
+		clock_out : OUT STD_LOGIC); -- Clock_out = 1Khz
+END Clock_Divider;
+
+ARCHITECTURE bhv OF Clock_Divider IS 
+
+	SIGNAL count : INTEGER := 1;
+	SIGNAL tmp : STD_LOGIC := '0';
+
+BEGIN
+
+	PROCESS (clk, reset)
+	BEGIN
+		IF (reset = '1') THEN
+			count <= 1;
+			tmp <= '0';
+		ELSIF (clk'event AND clk = '1') THEN
+			count <= count + 1;
+			IF (count = 25000) THEN -- to get 1Khz from 50 Mhz we need to divide by 50Khz.
+				tmp <= NOT tmp;     -- 
+				count <= 1;
+			END IF;
+		END IF;
+		clock_out <= tmp;
+	END PROCESS;
+
+END bhv;
