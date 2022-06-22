04713c070f253a728f3361c035d125f9699dc161
firas abdel ghani <firasgany7@gmail.com>
2022-06-22 20:54:15 +0300

    STABLE SYSTEM - Voltage Drop - Non-DSx Design
    Based on TNSR.I22.PRD.0.0.0.1
    
    GPP_B14 (Top swap override configured: 1.8V) -  SOC_SPKR Strap PU & PD In Tensor are NOP and connected to +3V3A rail.
    GPP_B23 (for this configuration keep 1.8V) - SML1ALERT#_PCHHOT#_GSPI1_CS1# Strap PU & PD In Tensor are NOP and connected to +3V3A rail and V1.8A rail.
    GPP_C2 (for this configuration keep 1.8V) - SMB_ALERT# strap connected to 4.75K PU to +V1.8A.
    GPP_C5 (for this configuration keep 1.8V) - GPP_C5/SML0ALERT# Strap PU & PD In Tensor are NOP and connected to +3V3A rail.
    GPP_E0 (for this configuration keep 1.8V) - Note: SATAXPCIE0 connected also to FPGA through NOP.
    GPIO_FPGA_SoC_1,2,3,4 : GPP_E11,E1,E12,E13  (for this option change configuration to 3.3V) - Inputs to FPGA.
    GPP_E2:  (for this option change configuration to 3.3V) - TPM_PIRQ# connected to 10K PU TO +3V3A.
    GPP_E6:  (for this option change configuration to 3.3V) - GPPC_E6_JTAG_ODT_DIS connected to 100K PU to +3V3A (in CRB it's connected to 1.8A)
    DSx: Disabled
    
    JTAG Power Disable: JTAG Power on C10 and Lower
    PCH Clock Output stable to PROCPWRGD high (tpch45) : 1ms
    PROCPWRGD and SYS_PWROK hgih to SUS_STAT de-assertion (tPCH46) : 2ms
    APWROK TIMING 2 ms
    
    INTERNAL PCH BUSES:
    eSPI Configuration:
    eSPI / EC : CRC CHECK ENABLED: NO!!
    Download and Execute: DnX Disabled
    FW Update Image Build: Thunderbolt/USB4: Disabled.
    ALL preotection disabled.
    
    reached: 0x00214a7d on first fall.
    reached: 0x00214a7d on second fall.

--- Lattice FPGA Works/TensorI22/dsw_pwrok.vhd
+++ Lattice FPGA Works/TensorI22/dsw_pwrok.vhd
@@ -1,22 +1,6 @@
 LIBRARY ieee;
 USE IEEE.std_logic_1164.ALL;
 USE IEEE.numeric_std.ALL;
--- tPCH02 in TL-PDG (p461/507) (V33DSW_OK -> DSW_PWROK), min: 10 ms, max: 2000 ms.
-
--- For the dead RTC coin cell and coin cell-less with depleted main battery RTC cases where the 3.3DSW ramps and powers the RTC well, 
--- the board designer should set the 3.3DSW rail stable to DSW_PWROK assertion delay (tPCH02) for = 30ms. 
--- This is required to ensure that SRTCRST# and RTCRST# de-assert after VCCRTC is stable, but before DSW_PWROK assertion. Failure to meet this requirement may result in DSW_PWROK
--- asserting with, or before, SRTCRST# and RTCRST# reach VIH, which is a sequencing violation and can result in a non-booting system scenario.
-
--- The SRTCRST# signal is used to reset the RTC registers used for the Intel® 
--- Management Engine (Intel® ME) when the on board battery is changed. 
--- The external capacitor and the external resistor between SRTCRST# and VccRTC were selected to create an RC time delay, 
--- such that RTCRST# will go high some time after the battery voltage is valid. The RC time delay should be in the range of 18–25 ms. 
--- There must not be a jumper for SRTCRST# pin. 
--- The SRTCRST# does not impact the implementation of CMOS clearing. Refer to Figure 63 on page 126 for external circuit for PCH RTC.
-
--- Complementary metal-oxide-semiconductor (CMOS) is a small amount of memory on a computer motherboard that stores the Basic Input/Output System (BIOS) settings. 
--- For no boot or no display issues, clearing CMOS may help recover the boards because that restores the BIOS default settings.
 
 ENTITY dsw_pwrok_block IS
 	PORT (
@@ -42,20 +26,18 @@ BEGIN
 		IF rising_edge(clk_100Khz) THEN
 			CASE curr_state IS
 
-				WHEN pwrgd => -- we stay at this state as long as (V33DSW_OK = '1'). 
+				WHEN pwrgd =>
 					IF (pwrok = '1') THEN
 						curr_state <= pwrgd;
 						DSW_PWROK  <= '1';
 					ELSE
-						curr_state <= no_pwrgd; -- Delay at DSW_PWROK transition from 0 to 1
-						DSW_PWROK  <= '0';      -- The DSW_PWROK signal will not assert at pwrok glitches of less then 1T
+						curr_state <= no_pwrgd; 
+						DSW_PWROK  <= '0';     
 					END IF;
 
-				WHEN delay =>                               --  After the 35 ms delay is finished we go to pwrgd state and otuput: DSW_PWROK <= '1'.
-					IF (count = to_unsigned(4000, 16)) THEN  --  3500 * 10uSec = 35 mSec.  Was: 1000 * 10uSec = 10 mSec
-					                                        --  TL-PDG: P.434 in Non-Dsx is connected to 3V3A.
-						                                    --  tPCH02 in TL-PDG (p461/507) (V33DSW_OK -> DSW_PWROK), min: 10 ms, max: 2000 ms.
-															-- Threshold is 1.8V. 
+				WHEN delay =>                              
+					IF (count = to_unsigned(4000, 16)) THEN  --  4000 * 10uSec = 40 mSec.  
+
 						curr_state <= pwrgd;
 						count      <= (OTHERS => '0');
 					ELSE
@@ -64,7 +46,7 @@ BEGIN
 					END IF;
 					   DSW_PWROK <= '0';
 
-				WHEN no_pwrgd => -- we start from this state.
+				WHEN no_pwrgd => 
 					IF (pwrok = '1') THEN
 						curr_state <= delay;
 						count      <= (OTHERS => '0');
