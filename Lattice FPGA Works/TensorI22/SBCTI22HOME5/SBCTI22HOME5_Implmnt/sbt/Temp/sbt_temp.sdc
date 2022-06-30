61f9444099507b5df1f87f4b2af8045f0818b0a0
firas abdel ghani <firasgany7@gmail.com>
2022-06-30 17:26:39 +0300

    Re-Arranged Folders

--- Lattice FPGA Works/TensorI22/SBCTI22HOME5/SBCTI22HOME5_Implmnt/sbt/Temp/sbt_temp.sdc
+++
@@ -1,3 +0,0 @@
-####---- CreateClock list ----1
-create_clock  -period 20.54 -waveform {0.00 10.27} -name {TOP|FPGA_OSC} [get_ports {FPGA_OSC}] 
-
--- Lattice FPGA Works/TensorI22/SBCTI22HOME5/synlog.tcl
+++
@@ -1,3 +0,0 @@
-history clear
-project -load SBCTI22HOME5_syn.prj
-project -close "C:/Users/firas/Dropbox/Board-Design/Board-Design-Git/Lattice FPGA Works/TensorI22/SBCTI22HOME5/SBCTI22HOME5_syn.prj"
